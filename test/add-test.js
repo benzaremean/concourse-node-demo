8b17b31bf03947ca25bc324b437edcf8e158fc00
Ben Ifie <ben.ifie@sky.uk>
2017-08-08 19:55:56 +0100

    commit again

--- test/add-test.js
+++ test/add-test.js
@@ -1,6 +1,8 @@
 const { assert } = require('chai');
 const add = require('../maths');
 
+const addition = 1;
+
 describe('tests', () => {
   it('two numbers added', () => {
     const answer = add(1, 1);
